ae9908f830973087072531c974ecb3948361af7e
somrat sorkar <themefisher.dev@gmail.com>
2024-01-16 08:38:01 +0600

    update live page

--- pages/live.js
+++ pages/live.js
@@ -1,16 +1,26 @@
 import Base from "@layouts/Baseof";
+import Link from "next/link";
 
 const Live = () => {
   return (
     <Base>
-      <div className="container py-24 text-center">
-        <iframe
-          className="aspect-video h-[50vh] w-full rounded lg:h-[600px]"
-          src="https://player.restream.io/?token=a716ed9131b44143b203c19056e1d4f0"
-          allow="autoplay"
-          allowfullscreen
-          frameborder="0"
-        ></iframe>
+      <div className="container">
+        <div className="lg:colo-8 mx-auto py-20 text-center">
+          <iframe
+            className="aspect-video h-[50vh] w-full rounded lg:h-[600px]"
+            src="https://player.restream.io/?token=a716ed9131b44143b203c19056e1d4f0"
+            allow="autoplay"
+            allowfullscreen
+          ></iframe>
+          <Link
+            href={"https://i9evkjegsiq.typeform.com/to/i1rwrmDp"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-light mt-12 inline-block hover:bg-light-primary"
+          >
+            Ready Player World ✨
+          </Link>
+        </div>
       </div>
     </Base>
   );
